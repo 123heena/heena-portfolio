385e1e34a5309fded8de9e0b22f287fd623f1f1b
Heena Kousar <kousar03.2003@gmail.com>
2026-06-01 21:08:56 +0530

    fix github pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
-
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-
-base:"/heena-portfolio/",
-
-plugins:[react(),tailwindcss()]
-
+  base: "/heena-portfolio/",
+  plugins: [react(), tailwindcss()],
 })
